0532dad596a8d4f591498c6d34a705140770a895
Denis Ibanez <denis.ibanez@outlook.com>
2020-07-26 20:52:17 +0800

    responsive ande desktop layouyt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-
 export class AppComponent implements OnInit {
   public title = 'tika-books-asteria';
+  public items: any = [
+    { url: '../../../assets/images/1.png', class: ''},
+    { url: '../../../assets/images/2.png', class: ''},
+    { url: '../../../assets/images/3.png', class: ''},
+    { url: '../../../assets/images/4.png', class: ''},
+    { url: '../../../assets/images/5.png', class: ''},    
+  ]
+  
   ngOnInit() {  
   }
 }
