05e1a3b0f22b9ddab42ce7ea24c40dd1313966d8
Denis Ibanez <denis.ibanez@outlook.com>
2020-07-29 21:28:57 +0800

    fix q.a document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
+  constructor() {}
+  
   public title = 'tika-books-asteria';
+  public msg = {
+    title: 'Lendo seu livro',
+    msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.'
+  }
   public items: any = [
     { url: '../../../assets/images/1.png', class: ''},
     { url: '../../../assets/images/2.png', class: ''},
@@ -17,6 +23,48 @@ export class AppComponent implements OnInit {
   ]
   
   ngOnInit() {  
+  }
+
+  onDomChange($event: Event): void {
+    const target = $event.target as HTMLTextAreaElement;
+    let ref:any;
+    if(target.classList.value == 'gallery-cell is-selected') {
+      ref = target.attributes[2].value
+      console.log(ref)
+
+      switch (ref) {
+        case 'index-0':
+          this.msg = { 
+            title: 'title 1',
+            msg: 'msg 1'
+          }
+          break;
+        case 'index-1':
+          this.msg = { 
+            title: 'title 2',
+            msg: 'msg 2'
+          }
+          break;
+        case 'index-2':
+          this.msg = { 
+            title: 'title 3',
+            msg: 'msg 3'
+          }
+            break;
+        case 'index-3':
+          this.msg = { 
+            title: 'title 4',
+            msg: 'msg 4'
+          }
+          break;
+        case 'index-4':
+          this.msg = { 
+            title: 'title 5',
+            msg: 'msg 5'
+          }
+          break;          
+      }
+    }
   }
 }
 
